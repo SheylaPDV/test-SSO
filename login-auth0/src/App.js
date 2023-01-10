7ed91c9cecfee2a62f45bba7831de7aa7f6017cb
SheylaPDV <92029579+SheylaPDV@users.noreply.github.com>
2023-01-10 13:23:02 +0100

    compruebo variables de entorno

--- login-auth0/src/App.js
+++ login-auth0/src/App.js
@@ -1,6 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
+
+console.log(
+  process.env.REACT_APP_AUTH0_DOMAIN,
+  "&",
+  process.env.REACT_APP_AUTH0_CLIENT_ID
+);
 
 function App() {
   return (
