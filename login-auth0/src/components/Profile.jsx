990fdd957c04c3f285d14446f7af5d602225837e
SheylaPDV <92029579+SheylaPDV@users.noreply.github.com>
2023-01-11 12:09:17 +0100

    cambios

--- login-auth0/src/components/Profile.jsx
+++ login-auth0/src/components/Profile.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import JSONPretty from "react-json-pretty";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
@@ -11,7 +10,7 @@ const Profile = () => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <JSONPretty data={user} />
+        <pre>{JSON.stringify(user)}</pre>
       </div>
     )
   );
